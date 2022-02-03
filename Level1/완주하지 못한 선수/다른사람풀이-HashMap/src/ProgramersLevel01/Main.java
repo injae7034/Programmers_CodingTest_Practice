3b0e1973877de350f413091a107bcd0e0edea46e
injae <injae7034@gmail.com>
2022-02-03 19:17:15 +0900

    코드 변경 및 테스트코드 추가

--- Level1/완주하지 못한 선수/다른사람풀이-HashMap/src/ProgramersLevel01/Main.java
+++ Level1/완주하지 못한 선수/다른사람풀이-HashMap/src/ProgramersLevel01/Main.java
@@ -1,5 +1,6 @@
 package ProgramersLevel01;
 
+import java.util.Iterator;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -21,11 +22,25 @@ public class Main {
             playerMap.put(player, playerMap.get(player) - 1);
         }
         //4. value가 0이 아닌 완주하지 못한 선수를 찾는다.
+        /*
+        //keySet 이용
         for(String key : playerMap.keySet())
         {
             if(playerMap.get(key) != 0)
             {
                 answer = key;
+                break;
+            }
+        }
+         */
+        //Iterator, Entry 이용
+        Iterator<Map.Entry<String, Integer>> iterator = playerMap.entrySet().iterator();
+        while (iterator.hasNext())
+        {
+            Map.Entry<String, Integer> entry = iterator.next();
+            if(entry.getValue() != 0)
+            {
+                answer = entry.getKey();
                 break;
             }
         }
@@ -35,6 +50,12 @@ public class Main {
     public static void main(String[] args) {
         String[] participant = {"leo", "kiki", "eden"};
         String[] completion = {"kiki", "eden"};
+        System.out.println(solution(participant, completion));
+        participant = new String[]{"marina", "josipa", "nikola", "vinko", "filipa"};
+        completion = new String[]{"josipa", "filipa", "marina", "nikola"};
+        System.out.println(solution(participant, completion));
+        participant = new String[]{"mislav", "stanko", "mislav", "ana"};
+        completion = new String[]{"stanko", "ana", "mislav"};
         System.out.println(solution(participant, completion));
     }
 }
