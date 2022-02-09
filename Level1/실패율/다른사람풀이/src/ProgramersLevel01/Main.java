86e09b905ae64898fa527970e31df1bd1ce8fe40
injae <injae7034@gmail.com>
2022-02-09 12:29:42 +0900

    다른 사람풀이 수정

--- Level1/실패율/다른사람풀이/src/ProgramersLevel01/Main.java
+++ Level1/실패율/다른사람풀이/src/ProgramersLevel01/Main.java
@@ -3,9 +3,10 @@ package ProgramersLevel01;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Arrays;
 
 public class Main {
-    public int[] solution(int N, int[] stages)
+    public static int[] solution(int N, int[] stages)
     {
         int playerCount = stages.length;
         int[] stagePlayers = new int[N + 2];
@@ -32,7 +33,7 @@ public class Main {
         }
         return answer;
     }
-    class Stage implements Comparable<Stage>
+    static class Stage implements Comparable<Stage>
     {
         public int level;
         public double failure;
@@ -56,8 +57,38 @@ public class Main {
             }
             return 0;
         }
+/*
+//level도 오름차순말고 내림차순으로 정렬하고 싶은 경우에 필요한 처리
+        @Override
+        public int compareTo(Stage one)
+        {
+            int ret = 0;
+            if(this.failure < one.failure)
+            {
+                ret = 1;
+            }
+            else if(this.failure > one.failure)
+            {
+                ret = -1;
+            }
+            else
+            {
+                if(this.level < one.level)
+                {
+                    ret = 1;
+                }
+                else if(this.level > one.level)
+                {
+                    ret = -1;
+                }
+            }
+            return ret;
+        }
+ */
     }
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        int[] answer = solution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3});
+        System.out.println(Arrays.toString(answer));
     }
 }
