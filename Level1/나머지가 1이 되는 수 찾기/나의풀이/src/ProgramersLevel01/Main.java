e9a61898b17aa7388eadd72f1824ce276bea486b
injae <injae7034@gmail.com>
2022-02-10 09:53:49 +0900

    새로운 문제 풀이

---
+++ Level1/나머지가 1이 되는 수 찾기/나의풀이/src/ProgramersLevel01/Main.java
@@ -0,0 +1,22 @@
+package ProgramersLevel01;
+
+public class Main {
+    public static int solution(int n) {
+        int answer = 0;
+        for(int i = 2; i < n; i++)
+        {
+            if(n % i == 1)
+            {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+	    int answer = solution(10);
+        System.out.println(answer);
+        answer = solution(12);
+        System.out.println(answer);
+    }
+}
