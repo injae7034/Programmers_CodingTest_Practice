78b1168468cd91f3175296bd1ecc303c5525c737
injae <injae7034@gmail.com>
2022-02-22 18:04:44 +0900

    테스트케이스추가

--- Level2/위장/다른사람풀이/src/ProgramersLevel02/Main.java
+++ Level2/위장/다른사람풀이/src/ProgramersLevel02/Main.java
@@ -29,5 +29,9 @@ public class Main {
         answer = solution(new String[][]{{"crowmask", "face"},
                 {"bluesunglasses", "face"}, {"smoky_makeup", "face"}});
         System.out.println(answer);
+        answer = solution(new String[][]{{"A", "heargear"}, {"B", "headgear"}, {"C", "face"},
+                {"D", "face"}, {"E", "face"}, {"F", "top"}, {"G", "bottom"}, {"H", "bottom"},
+                {"I", "bottom"}, {"J", "bottom"}});
+        System.out.println(answer);
     }
 }
